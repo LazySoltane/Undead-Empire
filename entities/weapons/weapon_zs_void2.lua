b894674653f088f8066183aed8522026a617bd1f
Lazy <callumharris1997@yahoo.co.uk>
2021-09-25 08:13:59 +0100

    void shotty now working, secondary fire may break it. drop it and pick it back up if that happens.

--- entities/weapons/weapon_zs_void2.lua
+++ entities/weapons/weapon_zs_void2.lua
@@ -4,21 +4,25 @@ SWEP.Base = "weapon_zs_baseshotgun"
 DEFINE_BASECLASS("weapon_zs_baseshotgun")
 
 SWEP.PrintName = "'Void' Martini's Shotgun"
-SWEP.Description = "4 pellets dealing 34+ damage in an 180 degree arc."
+SWEP.Description = "6 pellets dealing 45+ damage in an 180 degree arc."
 
 SWEP.UseHands = true
+
+if CLIENT then
+	
 
 SWEP.HoldType = "shotgun"
 SWEP.ViewModelFOV = 70
 SWEP.ViewModelFlip = false
-SWEP.ViewModel = "models/weapons/c_shotgun.mdl"
-SWEP.WorldModel = "models/weapons/w_shotgun.mdl"
+SWEP.ViewModel = "models/weapons/cstrike/c_shot_m3super90.mdl"
+SWEP.WorldModel = "models/weapons/w_shot_m3super90.mdl"
 SWEP.ShowViewModel = true
 SWEP.ShowWorldModel = true
 SWEP.ViewModelBoneMods = {
 	["ValveBiped.Bip01_Spine4"] = { scale = Vector(1, 1, 1), pos = Vector(0, -0.441, 0.089), angle = Angle(0, 0, 0) },
 	["ValveBiped.Gun"] = { scale = Vector(0.01, 0.01, 0.01), pos = Vector(0, 0.5, -0.9), angle = Angle(0, 0, 0) }
 }
+
 SWEP.VElements = {
 	["base"] = { type = "Model", model = "models/hunter/tubes/tube1x1x1.mdl", bone = "ValveBiped.Gun", rel = "", pos = Vector(1.066, -0.475, 21.307), angle = Angle(0, 0, 179.996), size = Vector(0.038, 0.038, 0.786), color = Color(109, 109, 109, 255), surpresslightning = false, material = "METAL2", skin = 0, bodygroup = {} },
 	["base+"] = { type = "Model", model = "models/hunter/tubes/tube1x1x1.mdl", bone = "ValveBiped.Gun", rel = "base", pos = Vector(-1.73, 0, 0), angle = Angle(0, 0, 0), size = Vector(0.038, 0.038, 0.786), color = Color(109, 109, 109, 255), surpresslightning = false, material = "METAL2", skin = 0, bodygroup = {} },
@@ -44,7 +48,7 @@ SWEP.VElements = {
 	["base+++++++++++++++++++++"] = { type = "Model", model = "models/props_phx/construct/metal_wire_angle360x2.mdl", bone = "ValveBiped.Gun", rel = "base", pos = Vector(0.009, 0, 2.521), angle = Angle(0, 0, 0), size = Vector(0.02, 0.02, 0.067), color = Color(109, 109, 109, 255), surpresslightning = false, material = "METAL2", skin = 0, bodygroup = {} },
 	["base++++++++++++++++++++++"] = { type = "Model", model = "models/props_junk/cardboard_box001a.mdl", bone = "ValveBiped.Gun", rel = "base", pos = Vector(-0.842, -1.291, 10.456), angle = Angle(0, 0, 0), size = Vector(0.034, 0.051, 0.403), color = Color(109, 109, 109, 255), surpresslightning = false, material = "METAL2", skin = 0, bodygroup = {} },
 	["base+++++++++++++++++++++++"] = { type = "Model", model = "models/props_phx/construct/metal_wire_angle360x2.mdl", bone = "ValveBiped.Gun", rel = "base", pos = Vector(-0.9, -1.868, 24.121), angle = Angle(0, 0, 0), size = Vector(0.018, 0.022, 0.072), color = Color(242, 125, 0, 255), surpresslightning = false, material = "METAL2", skin = 0, bodygroup = {} }
-	}
+}
 	
 SWEP.HUD3DBone = "ValveBiped.Gun"
 SWEP.HUD3DPos = Vector(2.8, 0, -8)
@@ -54,39 +58,31 @@ end
 
 SWEP.Base = "weapon_zs_baseshotgun"
 
-SWEP.HoldType = "shotgun"
+SWEP.ReloadDelay = 0.4
 
-SWEP.ViewModel = "models/weapons/cstrike/c_shot_m3super90.mdl"
-SWEP.WorldModel = "models/weapons/w_shot_m3super90.mdl"
-SWEP.UseHands = true
+SWEP.Primary.Sound = Sound("weapons/xm1014/xm1014-1.wav", 100, math.random(0, 70), 0.5, CHAN_WEAPON - 70)
+SWEP.Primary.Damage = 40
+SWEP.Primary.NumShots = 6
+SWEP.Primary.Delay = 0.8
 
-SWEP.ShowViewModel = false
-SWEP.ShowWorldModel = false
 
-SWEP.ReloadDelay = 0.45
-
-SWEP.Primary.Sound = Sound("Weapon_M3.Single")
-SWEP.Primary.Damage = 34
-SWEP.Primary.NumShots = 4
-SWEP.Primary.Delay = 0.2
-
-SWEP.Primary.ClipSize = 6
-SWEP.Primary.Automatic = true
+SWEP.Primary.ClipSize = 8
+SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = "buckshot"
 GAMEMODE:SetupDefaultClip(SWEP.Primary)
 
-SWEP.RequiredClip = 0
+SWEP.RequiredClip = 1
+SWEP.ReloadSound = Sound("Weapon_Shotgun.Reload")
 
-SWEP.ConeMax = 7.55
-SWEP.ConeMin = 5.25
+SWEP.ConeMax = 4.55
+SWEP.ConeMin = 3.25
 
 SWEP.FireAnimSpeed = 1
-SWEP.WalkSpeed = SPEED_SLOWER
-
 SWEP.Tier = 2
 
 GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_SHOT_COUNT, 1)
-GAMEMODE:AttachWeaponModifier(SWEP, WEAPON_MODIFIER_RELOAD_SPEED, 0.1, 1)
+
+
 
 function SWEP:PrimaryAttack()
 	self.AttackContext = true
@@ -94,13 +90,33 @@ function SWEP:PrimaryAttack()
 end
 
 function SWEP:SecondaryAttack()
-	self.AttackContext = nil
-	BaseClass.PrimaryAttack(self)
+	if not self:CanPrimaryAttack() then return end
+
+	local multiplier = self:Clip1()
+
+	self.Primary.NumShots = self.Primary.NumShots / multiplier
+	self.RequiredClip = 2
+	self.OldEmitFireSound = self.EmitFireSound
+	self.EmitFireSound = self.EmitFireSoundDouble
+
+	self:PrimaryAttack()
+
+	self.Primary.NumShots = self.Primary.NumShots / multiplier
+	self.RequiredClip = 1
+	self.EmitFireSound = self.OldEmitFireSound
 end
 
 function SWEP:EmitFireSound()
 	self:EmitSound("weapons/m3/m3-1.wav", 75, math.random(134, 136), 0.7)
 	self:EmitSound("weapons/xm1014/xm1014-1.wav", 75, math.random(172, 180), 0.5, CHAN_WEAPON + 20)
+end
+
+function SWEP:EmitFireSoundDouble()
+	if self:Clip1() == 2 then
+		self:EmitSound(self.Primary.Sound, 80, math.random(80, 85), 1, CHAN_WEAPON + 20)
+	else
+		self:OldEmitFireSound()
+	end
 end
 
 function SWEP:ShootBullets(dmg, numbul, cone)
